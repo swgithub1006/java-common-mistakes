0bdbfb931d8e4104a83cb0db1b3fcd25d79ba0f1
songwei <songwei@swlab>
2020-04-26 11:45:20 +0800

    fix 调整配置，依赖的jar包

--- src/main/java/org/geekbang/time/commonmistakes/springpart1/aopmetrics/TestAspectWithOrder20.java
+++ src/main/java/org/geekbang/time/commonmistakes/springpart1/aopmetrics/TestAspectWithOrder20.java
@@ -16,22 +16,21 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class TestAspectWithOrder20 {
 
-    @Before("execution(* org.geekbang.time.commonmistakes.springpart1.aopmetrics.TestController.*(..))")
-    public void before(JoinPoint joinPoint) throws Throwable {
-        log.info("TestAspectWithOrder20 @Before");
-    }
+	@Before("execution(* org.geekbang.time.commonmistakes.springpart1.aopmetrics.TestController.*(..))")
+	public void before(JoinPoint joinPoint) throws Throwable {
+		log.info("TestAspectWithOrder20 @Before");
+	}
 
-    @After("execution(* org.geekbang.time.commonmistakes.springpart1.aopmetrics.TestController.*(..))")
-    public void after(JoinPoint joinPoint) throws Throwable {
-        log.info("TestAspectWithOrder20 @After");
-    }
+	@After("execution(* org.geekbang.time.commonmistakes.springpart1.aopmetrics.TestController.*(..))")
+	public void after(JoinPoint joinPoint) throws Throwable {
+		log.info("TestAspectWithOrder20 @After");
+	}
 
-    @Around("execution(* org.geekbang.time.commonmistakes.springpart1.aopmetrics.TestController.*(..))")
-    public Object around(ProceedingJoinPoint pjp) throws Throwable {
-        log.info("TestAspectWithOrder20 @Around before");
-        Object o = pjp.proceed();
-        log.info("TestAspectWithOrder20 @Around after");
-        return o;
-    }
+	@Around("execution(* org.geekbang.time.commonmistakes.springpart1.aopmetrics.TestController.*(..))")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		log.info("TestAspectWithOrder20 @Around before");
+		Object o = pjp.proceed();
+		log.info("TestAspectWithOrder20 @Around after");
+		return o;
+	}
 }
-
--- src/main/java/org/geekbang/time/commonmistakes/springpart1/aopmetrics/UserService.java
+++ src/main/java/org/geekbang/time/commonmistakes/springpart1/aopmetrics/UserService.java
@@ -1,25 +1,26 @@
 package org.geekbang.time.commonmistakes.springpart1.aopmetrics;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Slf4j
+// @Slf4j
 @Metrics(ignoreException = true)
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
 
-    @Transactional
-    public void createUser(UserEntity entity) {
-        userRepository.save(entity);
-        if (entity.getName().contains("test"))
-            throw new RuntimeException("invalid username!");
-    }
+	@Autowired
+	private UserRepository userRepository;
 
-    public int getUserCount(String name) {
-        return userRepository.findByName(name).size();
-    }
+	@Transactional
+	public void createUser(UserEntity entity) {
+		userRepository.save(entity);
+		if (entity.getName().contains("test"))
+			throw new RuntimeException("invalid username!");
+	}
+
+	public int getUserCount(String name) {
+		return userRepository.findByName(name).size();
+	}
+
 }
